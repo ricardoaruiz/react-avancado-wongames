7e02e63127cc1bd2e16066f8c02818e105e03941
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-04-09 09:46:37 -0300

    Criando componente Logo (Parte 3)

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,9 +1,22 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Logo from './Logo'
 
+import { LogoProps } from './Logo'
+
 export default {
   title: 'Logo',
-  component: Logo
+  component: Logo,
+  argTypes: {
+    color: {
+      name: 'Color',
+      description: 'Text logo color',
+      defaultValue: 'black',
+      control: {
+        type: 'select',
+        options: ['white', 'black']
+      }
+    }
+  }
 } as Meta
 
-export const Basic: Story = (args) => <Logo {...args} />
+export const Basic: Story<LogoProps> = (args) => <Logo {...args} />
