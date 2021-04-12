055579a4445c5fcb2ec23547a6649185eb39c6a7
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-04-12 08:40:39 -0300

    Ajustando o botao para aceitar polimorfismo 'as'

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -6,12 +6,12 @@ import Button from './Button'
 
 describe('<Button />', () => {
   it('should render a medium button by default - snapshot', () => {
-    const { container } = renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button as="button">Buy now</Button>)
     expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render a medium button by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    renderWithTheme(<Button as="button">Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
@@ -21,7 +21,11 @@ describe('<Button />', () => {
   })
 
   it('should render a small button when size small is passed', () => {
-    renderWithTheme(<Button size="small">Buy now</Button>)
+    renderWithTheme(
+      <Button as="button" size="small">
+        Buy now
+      </Button>
+    )
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '3rem',
@@ -30,7 +34,11 @@ describe('<Button />', () => {
   })
 
   it('should render a large button when size large is passed', () => {
-    renderWithTheme(<Button size="large">Buy now</Button>)
+    renderWithTheme(
+      <Button as="button" size="large">
+        Buy now
+      </Button>
+    )
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '5rem',
@@ -40,7 +48,11 @@ describe('<Button />', () => {
   })
 
   it('should render a full width button when fullWidth is passed', () => {
-    renderWithTheme(<Button fullWidth>Buy now</Button>)
+    renderWithTheme(
+      <Button as="button" fullWidth>
+        Buy now
+      </Button>
+    )
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       width: '100%'
@@ -49,10 +61,25 @@ describe('<Button />', () => {
 
   it('should render a button with icon', () => {
     renderWithTheme(
-      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+      <Button as="button" icon={<AddShoppingCart data-testid="icon" />}>
+        Buy now
+      </Button>
     )
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render button as ling', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
   })
 })
