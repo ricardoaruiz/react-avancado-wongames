f8e8d98c3bae243798fa4daf04eb91ab0a68e1f1
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-04-15 09:51:43 -0300

    Criando os testes para o Slider e o mock no jest para o matchMedia do browser

--- src/components/Slider/Slider.test.tsx
+++ src/components/Slider/Slider.test.tsx
@@ -1,12 +1,24 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from './Slider'
 
 describe('<Slider />', () => {
-  it('should render correctly - without snapshot', () => {
-    render(<Slider />)
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
-
