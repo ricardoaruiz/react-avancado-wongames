5025e023638666daa9631566e2d5f637c49a8719
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-04-29 16:10:05 -0300

    Adicionando o componente Gallery na tela de Game

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -18,6 +18,34 @@ export const getStaticProps: GetStaticProps<GameProps> = async () => {
         description:
           'Experience the epic space strategy games that redefined the RTS genre. Control your fleet and build an armada across more than 30 single-player missions.',
         price: 219
+      },
+      gallery: {
+        items: [
+          {
+            src: '/img/games/cyberpunk-1.jpg',
+            label: 'Gallery Image 1'
+          },
+          {
+            src: '/img/games/cyberpunk-2.jpg',
+            label: 'Gallery Image 2'
+          },
+          {
+            src: '/img/games/cyberpunk-3.jpg',
+            label: 'Gallery Image 3'
+          },
+          {
+            src: '/img/games/cyberpunk-4.jpg',
+            label: 'Gallery Image 4'
+          },
+          {
+            src: '/img/games/cyberpunk-5.jpg',
+            label: 'Gallery Image 5'
+          },
+          {
+            src: '/img/games/cyberpunk-6.jpg',
+            label: 'Gallery Image 6'
+          }
+        ]
       }
     }
   }
--- src/templates/Game/Game.tsx
+++ src/templates/Game/Game.tsx
@@ -1,14 +1,21 @@
 import { Base } from 'templates'
 
 import * as S from './Game.styles'
-import { GameInfo, GameInfoProps } from 'components'
+import {
+  GameInfo,
+  GameInfoProps,
+  GalleryProps,
+  Gallery,
+  MediaMatch
+} from 'components'
 
 export type GameProps = {
   cover: string
   gameInfo: GameInfoProps
+  gallery?: GalleryProps
 }
 
-export const Game = ({ cover, gameInfo }: GameProps) => (
+export const Game = ({ cover, gameInfo, gallery }: GameProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -16,6 +23,14 @@ export const Game = ({ cover, gameInfo }: GameProps) => (
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
+      {!!gallery && (
+        <MediaMatch greaterThan="medium">
+          <S.SectionGallery>
+            <Gallery {...gallery} />
+          </S.SectionGallery>
+        </MediaMatch>
+      )}
     </S.Main>
   </Base>
 )
