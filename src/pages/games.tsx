8ebe94001adc3bbfc0682ec3ca62ea8eea8240aa
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-06-05 15:28:21 -0300

    Instalando o codegen do Apollo e gerando as interfaces baseadas no schema

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -5,33 +5,16 @@ import { Games as GamesTemplate, GamesProps } from 'templates'
 import filterItems from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames } from 'graphql/generated/QueryGames'
 
 const GamesPage = (props: GamesProps) => {
   return <GamesTemplate {...props} />
 }
 
-type GameData = {
-  name: string
-  slug: string
-  cover: {
-    url: string
-  }
-  developers: [
-    {
-      name: string
-    }
-  ]
-  price: number
-}
-
-type GamesResponse = {
-  games: GameData[]
-}
-
 export const getStaticProps: GetStaticProps<GamesProps> = async () => {
   const client = initializeApollo()
 
-  const { data } = await client.query<GamesResponse>({
+  const { data } = await client.query<QueryGames>({
     query: QUERY_GAMES,
     variables: { limit: 9 }
   })
