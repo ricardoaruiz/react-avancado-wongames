b3ecfcffd13f71c0e5aa8a1e8f0067134fcff2e1
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-06-05 14:16:59 -0300

    Configurando o Apollo para funcionar tanto no cliente como em SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,30 @@
+import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import { useApollo } from 'utils/apollo'
 
 import GlobaStyles from 'styles/global'
 import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-192.png" />
-        <link rel="apple-touch-icon" href="/img/icon-192.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best game store in the world" />
-      </Head>
+  const client = useApollo()
 
-      <GlobaStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-192.png" />
+          <link rel="apple-touch-icon" href="/img/icon-192.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="description" content="The best game store in the world" />
+        </Head>
+
+        <GlobaStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
---
+++ src/utils/apollo.ts
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+// Creating apollo client
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  })
+}
+
+/**
+ * Initialize Apollo client
+ * @param initialState
+ * @returns
+ */
+export const initializeApollo = (initialState = {}) => {
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  // recuperando os dados do cache
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  // sempre inicializando no SSR com cache limpo
+  if (typeof window === 'undefined') return apolloClientGlobal
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+/**
+ * Hook para utilização do apollo client
+ * @param initialState
+ * @returns
+ */
+export const useApollo = (initialState = {}) => {
+  const store = React.useMemo(() => initializeApollo(initialState), [
+    initialState
+  ])
+
+  return store
+}
